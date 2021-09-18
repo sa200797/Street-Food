d7d798b7a18d3ca5da0981ef83f1a0583b7e76ea
shubham20t <agrawalshubhamop@gmail.com>
2021-09-18 14:58:49 +0530

    Vadatest0.1

--- Assets/Scripts/DropCounter.cs
+++ Assets/Scripts/DropCounter.cs
@@ -8,10 +8,17 @@ public class DropCounter : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("VadaPawComplete1");
-        if (collision.gameObject.CompareTag("Finish"))
+        if (collision.gameObject.CompareTag("C_Vadapav"))
         {
             Debug.Log("VadaPawComplete2");
-            GameManager.foodtemspawn = false;
+            GameManager.vadaitemspawn = false;
+            GameManager.vadapawcount = 0;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("C_Sandwich"))
+        {
+            Debug.Log("VadaPawComplete2");
+            GameManager.vadaitemspawn = false;
             GameManager.vadapawcount = 0;
             Destroy(collision.gameObject);
         }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,24 +2,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+
     public GameObject breadprefab, masalaprefab, vadapawprefab;
 
-    public int whatToSpawn;
-    public Transform dropppoint;
+    public GameObject san_breadprefab, breadsauce_prefab, veggie_prefab, toast_prefab;
 
+
+
+
+    public int whatToSpawn;
+
+
+    public Transform vada_dropppoint;
+    public Transform san_droppoint; 
+    
     public GameObject vadapaw; // to delete th clones;
 
+    public GameObject sandwich;
 
-   public static bool foodtemspawn; //only to spawn vadapaw;
+
+   public static bool vadaitemspawn; //only to spawn vadapaw;
    public static int vadapawcount = 0; //to check the foodcount for vadapav;
 
+
+    public static bool sandwichitemsspawn;
+    public static int sandwichcount=0;
+
+
+
+    private void Awake()
+    {
+        
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        foodtemspawn = false;
+        vadaitemspawn = false;
+        sandwichitemsspawn = false;
+
     }
 
     // Update is called once per frame
@@ -47,26 +81,33 @@ public class GameManager : MonoBehaviour
 
     //}
 
-    void GetInfo()
+    
+
+    public void GetInfo()
     {
         var ray = JMRPointerManager.Instance.GetCurrentRay();
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, maxDistance: 100))
         {
-            if (hit.collider.tag == "Player")
+            if (hit.collider.tag == "VadaPav")
             {
                 whatToSpawn = hit.collider.GetComponent<ItemCount>().Foodvalue;
                 MakeVadapaw();
                 //Debug.Log("Shubham");
+            }
+
+            if(hit.collider.tag =="Sandwich")
+            {
+                whatToSpawn = hit.collider.GetComponent<ItemCount>().Foodvalue;
+                MakeSandwich();
             }
         }
     }
 
     public void MakeVadapaw()
     {
-        
-
-        if (!foodtemspawn)
+       
+        if (!vadaitemspawn)
         {
             switch (whatToSpawn)
             {
@@ -74,7 +115,7 @@ public class GameManager : MonoBehaviour
                 case 1:
                     if(vadapawcount == 0)
                     {
-                        GameObject bomb = Instantiate(breadprefab, dropppoint.transform.position, Quaternion.identity);
+                        GameObject bomb = Instantiate(breadprefab, vada_dropppoint.transform.position, Quaternion.identity);
                         vadapaw = bomb;
                         vadapawcount++;
                         Debug.Log(vadapawcount);
@@ -84,7 +125,7 @@ public class GameManager : MonoBehaviour
                     if(vadapawcount == 1)
                     {
                         Destroy(vadapaw);
-                        GameObject bomb2 = Instantiate(masalaprefab, dropppoint.transform.position, Quaternion.identity);
+                        GameObject bomb2 = Instantiate(masalaprefab, vada_dropppoint.transform.position, Quaternion.identity);
                         vadapaw = bomb2;
                         vadapawcount++;
                         Debug.Log(vadapawcount);
@@ -94,7 +135,7 @@ public class GameManager : MonoBehaviour
                     if(vadapawcount== 2)
                     {
                         Destroy(vadapaw);
-                        Instantiate(vadapawprefab, dropppoint.transform.position, Quaternion.identity);
+                        Instantiate(vadapawprefab, vada_dropppoint.transform.position, Quaternion.identity);
                         vadapawcount++;
                         Debug.Log(vadapawcount);
 
@@ -108,7 +149,61 @@ public class GameManager : MonoBehaviour
 
             }
 
-       }    
+        }    
     }
-        
+    
+    
+    public void MakeSandwich()
+    {
+        if(!sandwichitemsspawn)
+        {
+            switch (whatToSpawn)
+            {
+
+                case 1:
+                    if (sandwichcount == 0)
+                    {
+                        GameObject bread = Instantiate(san_breadprefab, san_droppoint.transform.position, Quaternion.identity);
+                        sandwich = bread;
+                        sandwichcount++;
+                        
+                    }
+                    break;
+                case 2:
+                    if (sandwichcount == 1)
+                    {
+                        Destroy(sandwich);
+                        GameObject breadsauce = Instantiate(breadsauce_prefab, san_droppoint.transform.position, Quaternion.identity);
+                        sandwich = breadsauce;
+                        sandwichcount++;
+                        
+                    }
+                    break;
+                case 3:
+                    if (sandwichcount == 2)
+                    {
+                        Destroy(sandwich);
+                        GameObject veggeprefab= Instantiate(veggie_prefab, san_droppoint.transform.position, Quaternion.identity);
+                        sandwich = veggeprefab;
+                        sandwichcount++;
+                    }
+                    break; 
+                case 4:
+                    if (sandwichcount == 3)
+                    {
+                        Destroy(sandwich);
+                        GameObject toastsandwich = Instantiate(toast_prefab, san_droppoint.transform.position, Quaternion.identity);
+                        sandwich = toastsandwich;
+                        sandwichcount++;
+                        Debug.Log(vadapawcount);
+
+                    }
+                    break;
+                default:
+                    Debug.Log("Please follow the food itme menu");
+                    break;
+
+            }
+        }
+    }
 }
