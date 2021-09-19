50eb3629088c18f11ff36447a07e42cd147e2fb6
Apurva <paiapurva273@gmail.com>
2021-09-20 02:39:51 +0530

    Dirty hardcode to match objective with droppoint collider

--- Assets/Scripts/DropCounter.cs
+++ Assets/Scripts/DropCounter.cs
@@ -8,43 +8,57 @@ public class DropCounter : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("VadaPawComplete1");
-        if (collision.gameObject.CompareTag("C_Vadapav"))
+        if (collision.gameObject.CompareTag("C_Vadapav")&& GameManager.instance.vadapavText.text == "Vadapav x 1")
         {
             Debug.Log("VadaPawComplete2");
             GameManager.vadaitemspawn = false;
             GameManager.vadapawcount = 0;
             Destroy(collision.gameObject,3f);
-            Invoke("DestroyText", 2f);
             StartCoroutine(ChangeText("Sandwich x 1"));
             //GameManager.instance.vadapavText.text = "Sandwich x 1";
         }
-        if (collision.gameObject.CompareTag("C_Sandwich"))
+        else
+        {
+            GameManager.instance.vadapavText.text = "Wrong Recepie try again";
+            Destroy(collision.gameObject, 3f);
+            GameManager.instance.vadapavText.text = "Vadapav x 1";
+        }
+        if (collision.gameObject.CompareTag("C_Sandwich") && GameManager.instance.vadapavText.text == "Sandwich x 1")
         {
             Debug.Log("Sandwich Complete");
             GameManager.sandwichitemsspawn = false;
             GameManager.sandwichcount = 0;
             Destroy(collision.gameObject,3f);
             StartCoroutine(ChangeText("Pizza x 1"));
-
-
+        }
+        else
+        {
+            GameManager.instance.vadapavText.text = "Wrong Recepie try again";
+            Destroy(collision.gameObject, 3f);
+            GameManager.instance.vadapavText.text = "Sandwich x 1";
         }
 
-        if (collision.gameObject.CompareTag("C_Pizza"))
+        if (collision.gameObject.CompareTag("C_Pizza") && GameManager.instance.vadapavText.text == "Pizza x 1")
         {
             Debug.Log("Pizza Complete");
             GameManager.pizzaitemspawn = false;
             GameManager.pizzacount = 0;
             Destroy(collision.gameObject, 3f);
             StartCoroutine(ChangeText("Good Job!"));
-
-
+        }
+        else
+        {
+            GameManager.instance.vadapavText.text = "Wrong Recepie try again";
+            Destroy(collision.gameObject, 3f);
+            GameManager.instance.vadapavText.text = "Pizza x 1";
         }
     }
 
     IEnumerator ChangeText(string a)
     {
-        yield return new WaitForSeconds(3f);
         Debug.Log("corotune started");
+        GameManager.instance.vadapavText.text = "Completed";
+        yield return new WaitForSeconds(4f);
         GameManager.instance.vadapavText.text = a;
 
     }
