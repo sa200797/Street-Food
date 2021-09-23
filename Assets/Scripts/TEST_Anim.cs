58aa1e2c873d68df81e7c92b12356033cf5d372d
Apurva <paiapurva273@gmail.com>
2021-09-23 19:50:26 +0530

    Scene fade transition

--- Assets/Scripts/TEST_Anim.cs
+++ Assets/Scripts/TEST_Anim.cs
@@ -21,15 +21,22 @@ public class TEST_Anim : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Spacebar pressed");
             StartCoroutine(Fading());
-
         }
+    }
+
+    public void PlayAnimationInObject()
+    {
+        
     }
 
     IEnumerator Fading()
     {
         anim.SetBool("Fade", true);
+        Debug.Log("Coroutine Started");
         yield return new WaitUntil(() => black.color.a == 1);
         SceneManager.LoadScene(1);
+        
     }
 }
